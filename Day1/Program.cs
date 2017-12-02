bcd67455e45f68d2b124e38b3a4706def02bbd99
Adam Mahood <adam.m.mahood@gmail.com>
2017-12-01 22:15:45 -0800

    Initial solution for Day Part 1!

---
+++ Day1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Advent of Code Day 1 Challenge");
+
+            string puzzleInput = args[0];
+            Console.WriteLine("Your puzzle input is " + puzzleInput);
+        
+            //Validate input is all digits
+            if ( (puzzleInput.Length == 0) || (puzzleInput.Length==1) || (puzzleInput.All(char.IsDigit) == false) )
+            {
+                Console.WriteLine("Bad puzzle input :(, must be n>1 numbers...bailing");
+                return;
+            }
+
+            long outputSum = 0;
+
+            int i = 0;
+            while (i < (puzzleInput.Length-1))
+            {
+                if (puzzleInput[i] == puzzleInput[i+1])
+                {
+                    outputSum = outputSum + (puzzleInput[i]-'0');
+                }   
+                i++;             
+            }
+
+            if (puzzleInput[0] == puzzleInput.Last()) { outputSum = outputSum + puzzleInput.Last()-'0';}
+
+            Console.WriteLine("Puzzle instance answer = " + outputSum.ToString());
+        }
+    }
+}
