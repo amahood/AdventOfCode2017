bc232ac03261302210875254091b392c4f8c9edd
Adam Mahood <adam.m.mahood@gmail.com>
2017-12-08 21:06:41 -0800

    Solution for Day 7 Part 2, piece of junk solution, requires human intervention to read output

--- Unified/Day7.cs
+++ Unified/Day7.cs
@@ -27,6 +27,218 @@ namespace AdventOfCode
             Console.WriteLine("Number of trimmed tree elements - " + trimmedTree.Count);
             int indexOfBase = FindBase(trimmedTree);
             Console.WriteLine("Tree trunk - " + trimmedTree[indexOfBase].nodeRoot);
+
+            int indexInMainTree = FindNodeFromRootString(tree, trimmedTree[indexOfBase].nodeRoot);
+
+            List<TreeNode> weightedTree = WeightTree(tree, tree[indexInMainTree]);
+            PrintLayeredWeightedTree(weightedTree, weightedTree[FindNodeFromRootString(weightedTree,tree[indexInMainTree].nodeRoot)]);
+
+            //TODO/NOTE ON THE BELOW< EVEN WITH FUNCTION STUBS, I DON'T HAVE A GOOD WAY TO TRACK WEIGHT OF WAHT IT NEEDS TO BE, MAYBE DO IN FUNCTION
+            //FIND ELEMNT THAT IS DIFFERENT HTATN IT'S NEIGHBORS BUT ALL CHILDREN WEIGHT ARE THE SAME
+            int suspectIndex = 0;
+            int suspectNodeWeight = 0;
+            bool suspectFound = false;
+            string suspectNodeRoot = "";
+            foreach (TreeNode n in weightedTree)
+            {
+                if (!suspectFound)
+                {
+                    if (FindNodeFromRootString(weightedTree,  tree[indexInMainTree].nodeRoot)==0){}
+                    else if (AreNeighborsDifferentSameWeight(weightedTree, n) && AreChildrenSameWeight(weightedTree, n))
+                    {
+                        suspectIndex = FindNodeFromRootString(weightedTree, n.nodeRoot);
+                        suspectNodeWeight = n.nodeNumber;
+                        Console.WriteLine("Local Number - " + suspectNodeWeight);
+                        suspectFound = true;
+                        suspectNodeRoot = n.nodeRoot;
+                    }
+                }
+            }
+
+
+
+        }
+
+        public static bool AreNeighborsDifferentSameWeight(List<TreeNode> weightedTree, TreeNode node)
+        {                
+            //Find thing that points to me
+            Console.WriteLine("Analyzing node - " + node.nodeRoot);
+            bool pointToMeFound = false;
+            TreeNode nodePointsToMe = new TreeNode();
+            foreach (TreeNode tn in weightedTree)
+            {
+                if (tn.nodePointers.Contains(node.nodeRoot) && !pointToMeFound)
+                {
+                    nodePointsToMe = tn;
+                    pointToMeFound = true;
+                }
+            }
+            
+            //Find weight of all its children, need to ensure I am the differnet one
+            bool amDifferentThanNeighbors = false;
+            List<int> siblingWeights = new List<int>();
+
+            //Get weight of all my siblings
+            foreach (string s in nodePointsToMe.nodePointers)
+            {
+                int indexOfChild = FindNodeFromRootString(weightedTree, s);
+                siblingWeights.Add(weightedTree[indexOfChild].localWeight);
+                Console.WriteLine("Sibling Weight - " + weightedTree[indexOfChild].localWeight);
+            }
+
+            siblingWeights.Remove(node.localWeight);
+            if (!siblingWeights.Contains(node.localWeight))
+            {
+                amDifferentThanNeighbors = true;
+            }
+
+            return amDifferentThanNeighbors;
+        }
+
+        public static bool AreChildrenSameWeight(List<TreeNode> weightedTree, TreeNode node)
+        {
+            bool areChildrenSameWeight = true;
+            List<int> childweights = new List<int>();
+
+            //Get weight of all my children
+            foreach (string s in node.nodePointers)
+            {
+                int indexOfChild = FindNodeFromRootString(weightedTree, s);
+                childweights.Add(weightedTree[indexOfChild].localWeight);
+            }
+            
+            //Compare all
+            int comparisonWeight = childweights.First();
+            foreach (int i in childweights)
+            {
+                if (i != comparisonWeight)
+                {
+                    areChildrenSameWeight = false;
+                }
+            }
+
+            return areChildrenSameWeight;
+        }
+
+        public static List<TreeNode> WeightTree(List<TreeNode> inputTree, TreeNode root)
+        {
+            List<TreeNode> weightedTree = new List<TreeNode>();
+            int testCounter = 0;
+            foreach (TreeNode n in inputTree)
+            {
+                n.localWeight = FindWeight(inputTree, n);
+                //Console.WriteLine("Local weight - "+n.localWeight + " - " + testCounter);
+                weightedTree.Add(n);
+                testCounter++;
+            }
+
+            return weightedTree;
+        }
+
+        public static void PrintLayeredWeightedTree(List<TreeNode> tree, TreeNode root)
+        {
+            int layerTracker = 0;
+            List<TreeNode> previousLayer = new List<TreeNode>();
+            List<TreeNode> currentLayer = new List<TreeNode>();
+            bool childrenRemain = true;
+            previousLayer.Add(root);
+            Console.WriteLine("Layer " + layerTracker + " - " + previousLayer.First().localWeight);
+            layerTracker++;
+
+            while (childrenRemain)
+            {
+                bool atleastOneWithChild = false;
+                foreach (TreeNode n in previousLayer)
+                {
+                    Console.WriteLine("New node");
+                    if (n.nodePointers.Count>0)
+                    {
+                        atleastOneWithChild = true;
+                        foreach (string s in n.nodePointers)
+                        {
+                            TreeNode nextNode = tree[FindNodeFromRootString(tree, s)];
+                            currentLayer.Add(nextNode);
+                            Console.WriteLine("Layer " + layerTracker + " - " + nextNode.localWeight + " - " + nextNode.nodeRoot+ " - " + nextNode.nodeNumber);
+            
+
+
+                        }
+                    }
+                }
+                previousLayer.Clear();
+                foreach (TreeNode tn in currentLayer)
+                {
+                    TreeNode nodeCopy = tn;
+                    previousLayer.Add(nodeCopy);
+                }
+
+                //POTENTIALLY TRY TO DO IS SUSPECT IN CURRENT LAYER - NEED ELEMENT THAT IS DIFFERENT FROM NEIGHBORS BUT ALL CHILDREN ARE EQUAL
+                
+                {
+
+                }
+
+                currentLayer.Clear();
+                childrenRemain = atleastOneWithChild;
+                layerTracker++;
+            }
+                
+        }
+
+        
+
+        public static int FindNodeFromRootString(List<TreeNode> tree, string nodeRoot)
+        {
+            int indexTracker = 0;
+
+            foreach (TreeNode n in tree)
+            {
+                if (n.nodeRoot==nodeRoot)
+                {
+                    indexTracker = tree.IndexOf(n);
+                }
+            }
+            return indexTracker;
+        }
+
+        public static int FindWeight(List<TreeNode> tree, TreeNode currentNode)
+        {
+            int runningWeightSum = currentNode.nodeNumber;
+            if (currentNode.nodePointers.Count==0)
+            {
+                return currentNode.nodeNumber;
+            }
+            else
+            {
+                foreach (string s in currentNode.nodePointers)
+                {
+                    int indexOfNode = FindNodeFromRootString(tree, s);
+                    int currentWeight = FindWeight(tree, tree[indexOfNode]);
+                    runningWeightSum += currentWeight;
+                }
+                return runningWeightSum;
+            }
+        }
+
+        public static bool IsBalanced(List<TreeNode> tree, TreeNode currentNode, TreeNode rootNode)
+        {
+            bool balancedTracker = true;
+            int myWeight = currentNode.nodeNumber;
+            // At node, for each node pointer, is the weight equal
+            int comparisonWeight = 0;
+            int nodePoitnerTracker = 1;
+            foreach (string s in currentNode.nodePointers)
+            {
+                int indexOfNode = FindNodeFromRootString(tree, s);
+                int currentWeight = FindWeight(tree, tree[indexOfNode]);
+                if (nodePoitnerTracker==1){comparisonWeight=currentWeight;nodePoitnerTracker++;}
+                else 
+                {
+                    if (currentWeight+myWeight!=comparisonWeight){balancedTracker=false;}
+                    nodePoitnerTracker++;
+                }
+            }
+            return balancedTracker;
         }
 
         public static List<TreeNode> TrimTree(List<TreeNode> tree)
@@ -103,10 +315,12 @@ namespace AdventOfCode
             nodeRoot="";
             nodeNumber=0;
             nodePointers = new List<string>();
+            localWeight = 0;
         }
 
         public string nodeRoot;
         public int nodeNumber;
         public List<string> nodePointers;
+        public int localWeight;
     }
 }
