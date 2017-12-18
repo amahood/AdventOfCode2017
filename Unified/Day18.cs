9080301362e1398f5c9cb34214c9485f05f49891
Adam Mahood <adam.m.mahood@gmail.com>
2017-12-17 22:29:36 -0800

    Initial working commit for Day 17, no answer yet, but sample works

--- Unified/Day18.cs
+++ Unified/Day18.cs
@@ -13,7 +13,146 @@ namespace AdventOfCode
         {
             Console.WriteLine("-------------------DAY 18-------------------");
 
-            
+            List<Instruction> instructionSet = new List<Instruction>();
+
+            StreamReader sr = new StreamReader("day18Input.txt");
+            string line;
+            char delimiter = ' ';
+            while ((line = sr.ReadLine()) != null)
+            {
+                //Create new instruction and add
+                string[] substrings = line.Split(delimiter);
+                switch (substrings.Count())
+                { 
+                    case 1:
+                        Instruction newInst1 = new Instruction(substrings[0],' ',"");
+                        instructionSet.Add(newInst1);
+                        break;
+                    case 2:
+                        Instruction newInst2 = new Instruction(substrings[0],substrings[1][0],"");
+                        instructionSet.Add(newInst2);
+                        break;
+                    case 3:
+                        Instruction newInst3 = new Instruction(substrings[0],substrings[1][0],substrings[2]);
+                        instructionSet.Add(newInst3);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            //Create new register set
+            Dictionary<char,int> registerSet = new Dictionary<char, int>();
+
+            //Process all instructions until next instruction is either <0 or ># instructions
+            int nextInstructionIndex = 0;
+            int lastFrequencyPlayed = 0;
+            int numInstructionsProcessed = 0;
+            while (nextInstructionIndex>=0 && nextInstructionIndex<instructionSet.Count)
+            {
+                Instruction currentInstruction = instructionSet[nextInstructionIndex];
+
+                //If we haven't seen this register yet, initialize it and put it in the register set
+                if (!registerSet.ContainsKey(currentInstruction.operand1))
+                {
+                    registerSet.Add(currentInstruction.operand1,0);
+                }
+                //TODO - May need to check to see if operand2 existis, also, unless they have checked we will never try somethign we haven't seen
+
+                //Process instruction
+                int parsedNum;
+                switch (currentInstruction.instructionType)                
+                {
+                    case "snd":
+                        lastFrequencyPlayed = registerSet[currentInstruction.operand1];
+                        nextInstructionIndex++;
+                        break;
+                    case "set":
+                        if (Int32.TryParse(currentInstruction.operand2, out parsedNum))
+                        {
+                            registerSet[currentInstruction.operand1] = parsedNum;
+                        }
+                        else
+                        {
+                            registerSet[currentInstruction.operand1] = registerSet[currentInstruction.operand2[0]];
+                        }
+                        nextInstructionIndex++;
+                        break;
+                    case "add":
+                        if (Int32.TryParse(currentInstruction.operand2, out parsedNum))
+                        {
+                            registerSet[currentInstruction.operand1] = registerSet[currentInstruction.operand1]+parsedNum;
+                        }
+                        else
+                        {
+                            registerSet[currentInstruction.operand1] = registerSet[currentInstruction.operand1] + registerSet[currentInstruction.operand2[0]];
+                        }
+                        nextInstructionIndex++;
+                        break;
+                    case "mul":
+                        if (Int32.TryParse(currentInstruction.operand2, out parsedNum))
+                        {
+                            registerSet[currentInstruction.operand1] = registerSet[currentInstruction.operand1]*parsedNum;
+                        }
+                        else
+                        {
+                            registerSet[currentInstruction.operand1] = registerSet[currentInstruction.operand1] * registerSet[currentInstruction.operand2[0]];
+                        }
+                        nextInstructionIndex++;
+                        break;
+                    case "mod":
+                        if (Int32.TryParse(currentInstruction.operand2, out parsedNum))
+                        {
+                            registerSet[currentInstruction.operand1] = registerSet[currentInstruction.operand1]%parsedNum;
+                        }
+                        else
+                        {
+                            registerSet[currentInstruction.operand1] = registerSet[currentInstruction.operand1] % registerSet[currentInstruction.operand2[0]];
+                        }
+                        nextInstructionIndex++;
+                        break;
+                    case "rcv":
+                        if (registerSet[currentInstruction.operand1]!=0)
+                        {
+                            Console.WriteLine("Recovering last frequency - "+lastFrequencyPlayed);
+                        }
+                        nextInstructionIndex++;
+                        break;
+                    case "jgz":
+                        if (registerSet[currentInstruction.operand1]>0)
+                        {
+                            if (Int32.TryParse(currentInstruction.operand2, out parsedNum))
+                            {
+                                nextInstructionIndex+=parsedNum;
+                            }
+                            else
+                            {
+                                nextInstructionIndex+=registerSet[currentInstruction.operand2[0]];
+                            }
+                        }
+                        else {nextInstructionIndex++;}
+                        break;
+                    default:
+                        break;
+                }
+            numInstructionsProcessed++;
+            }
         }     
+
+        public class Instruction
+        {
+            public Instruction(string inst, char o1, string o2)
+            {
+                instructionType = inst;
+                operand1 = o1;
+                operand2 = o2;
+            }
+
+            public string instructionType;
+            public char operand1;
+            public string operand2;
+        }
     }
+
+    
 }
